e859cfda3a9107d2a818deea571aae0b5ce47e63
Nguyen Hai <nguyenhaipc98@gmail.com>
2025-07-06 22:35:43 +0300

    add port to file .properties

--- src/main/java/com/example/carpetshop/config/SecurityConfig.java
+++ src/main/java/com/example/carpetshop/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,6 +22,8 @@ import java.util.List;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+
 public class SecurityConfig {
 
     @Bean
