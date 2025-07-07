a63297327137b84a0f11f816928465e686821514
Nguyen Hai <nguyenhaipc98@gmail.com>
2025-07-07 11:00:36 +0300

    fix path redirect Oath2 success

--- src/main/java/com/example/carpetshop/security/OAuth2SuccessHandler.java
+++ src/main/java/com/example/carpetshop/security/OAuth2SuccessHandler.java
@@ -60,7 +60,7 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
 
         // Redirect về frontend
-        String redirectUrl = "https://carpetshop.netlify.apps/oauth-success?token=" + token;
+        String redirectUrl = "https://carpetshop.netlify.app/oauth-success?token=" + token;
         response.sendRedirect(redirectUrl);
     }
 }
